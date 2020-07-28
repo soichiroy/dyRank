059f6fd27bdf43aef218f96ee44377ce4ffbf147
sou412 <sou412snow@gmail.com>
2020-07-28 10:07:06 -0400

    Update scripts

--- data-raw/import_data.R
+++ data-raw/import_data.R
@@ -6,13 +6,14 @@
 
 path <- "~/Dropbox/research/projects/formula_one/data_clean"
 
-
+## package
 require(tidyverse)
 
-driver_long <- read_rds(paste(path, "driver_long.rds", sep = "/"))
-driver_wide <- read_rds(paste(path, "driver_wide.rds", sep = "/"))
+## load data 
+f1_driver_long <- read_rds(paste(path, "driver_long.rds", sep = "/"))
+f1_driver <- read_rds(paste(path, "driver_wide.rds", sep = "/"))
 
 
 ## export 
-usethis::use_data(driver_long, overwrite = TRUE)
-usethis::use_data(driver_wide, overwrite = TRUE)
+usethis::use_data(f1_driver_long, overwrite = TRUE)
+usethis::use_data(f1_driver, overwrite = TRUE)
