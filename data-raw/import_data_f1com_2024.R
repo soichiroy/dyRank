f63405acfa859ae76d5235dc2fab967fcd6d9a11
Soichiro Yamauchi <sou412@users.noreply.github.com>
2024-12-20 02:15:47 +0000

    Refactor data import and processing for race results, lap times, and starting grid

--- data-raw/import_data_f1com_2024.R
+++ data-raw/import_data_f1com_2024.R
@@ -1,18 +1,31 @@
 library(tidyverse)
-dat <- read_rds("data-raw/f1_8424_race_results.rds")
+
+# Load data
+dat_race <- read_rds("data-raw/f1_8424_race_results.rds")
+dat_grid <- read_rds("data-raw/f1_8424_starting_grid.rds")
+dat_lap <- read_rds("data-raw/f1_8424_fastest_laps.rds")
+
+# Function to process data
+ProcessData <- function(dat) {
+  dat_list <- vector('list', length = nrow(dat))
+  vars <- dat %>% select(-data)
+  for (i in 1:nrow(dat)) {
+    dat_list[[i]] <- dat$data[[i]] %>% 
+      mutate(Pos = as.numeric(Pos)) %>% 
+      select(Pos, driver = driver_name, abb_name, Car) %>% 
+      mutate(vars[i,])
+  }
+  out <- bind_rows(dat_list) %>%
+    rename(GP = race_name) %>%
+    mutate(driver = stringr::str_squish(driver))
+  return(out)
+}
 
 ## race 
-dat_long <- dat %>% filter(rank_type == "race-result")
-dat_list <- vector('list', length = nrow(dat_long))
-vars <- dat_long %>% select(-data)
-for (i in 1:nrow(dat_long)) {
-  dat_list[[i]] <- dat_long$data[[i]] %>% 
-    mutate(Pos = as.numeric(Pos)) %>% 
-    select(Pos, driver = driver_name, abb_name, Car) %>% 
-    mutate(vars[i,])
-}
-f1_race_8424 <- bind_rows(dat_list) %>%
-  rename(GP = race_name) %>%
-  mutate(driver = stringr::str_squish(driver))
+f1_race_8424 <- ProcessData(dat_race)
+f1_laptime_8424 <- ProcessData(dat_lap)
+f1_grid_8424 <- ProcessData(dat_grid)
 
 usethis::use_data(f1_race_8424, overwrite = TRUE)
+usethis::use_data(f1_laptime_8424, overwrite = TRUE)
+usethis::use_data(f1_grid_8424, overwrite = TRUE)
